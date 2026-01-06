d67ccbe8e324f9c3fdbbe5147af84aebd3b503e9
b-harshit <hbcodes07@gmail.com>
2026-01-06 17:59:51 +0530

    more pages

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useState,useRef,useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./Header.css";
 
 function Header() {
+  const [showEvents, setShowEvents] = useState(false);
+  const dropdownRef = useRef(null);
+
+   // Close dropdown when clicking outside
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (
+        dropdownRef.current &&
+        !dropdownRef.current.contains(event.target)
+      ) {
+        setShowEvents(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }, []);
+
   return (
     <header className="header">
       <div className="header-container">
@@ -15,7 +33,31 @@ function Header() {
           <ul>
             <li><Link to="/company">Company</Link></li>
             <li><Link to="/products">Products</Link></li>
-            <li><Link to="/events">Events</Link></li>
+            {/* EVENTS CLICK DROPDOWN */}
+            <li className="dropdown" ref={dropdownRef}>
+              <span
+                className="dropdown-title"
+                onClick={() => setShowEvents(!showEvents)}
+              >
+                Events
+              </span>
+
+              {showEvents && (
+                <div className="dropdown-menu">
+                  <Link to="/events/scientific-conferences">
+                    Scientific Conferences
+                  </Link>
+
+                  <Link to="/events/game-changer-workshop">
+                    Game Changer Workshop
+                  </Link>
+
+                  <Link to="/events/news">
+                    News
+                  </Link>
+                </div>
+              )}
+            </li>
             <li><Link to="/awards">Awards</Link></li>
             <li><Link to="/career">Career</Link></li>
             <li><Link to="/contact">Contact</Link></li>
